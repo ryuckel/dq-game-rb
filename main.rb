c7e6e85f410b568f6728f00c700bf5b8cf94986b
ryusuke.miyaji <bluce826@gmail.com>
2020-04-30 22:18:18 +0900

    add method to brave

--- main.rb
+++ main.rb
@@ -1,3 +1,28 @@
 class Brave
 
+  def name=(name)
+    @name = name
+  end
+
+  def hp=(hp)
+    @hp = hp
+  end
+
+  def offense=(offense)
+    @offense = offense
+  end
+
+  def defense=(defense)
+    @defense = defense
+  end
+
 end
+
+# 勇者クラスをインスタンス化
+brave = Brave.new
+
+# パラメータに値を設定
+brave.name = "テリー"
+brave.hp = 500
+brave.offense = 150
+brave.defense = 100
