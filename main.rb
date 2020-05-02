e32ccf5b09a0a3211d5d312ee7046eb5d4ae52a0
ryusuke.miyaji <bluce826@gmail.com>
2020-05-03 01:03:23 +0900

    make character

--- main.rb
+++ main.rb
@@ -1,15 +1,4 @@
-class Character
-  attr_reader :offense, :defense
-  attr_accessor :hp, :name
-
-  def initialize(**params)
-    @name = params[:name]
-    @hp = params[:hp]
-    @offense = params[:offense]
-    @defense = params[:defense]
-  end
-end
-
+require './character'
 class Brave < Character
   SPECIAL_ATTACK_CONSTANT = 1.5
 
